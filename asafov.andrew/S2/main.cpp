af30b40e4c56e85a9e67123333873c11f20a5821
shycoshy <neforscist@gmail.com>
2025-04-08 03:02:12 +0000

    tests no.10

--- asafov.andrew/S2/main.cpp
+++ asafov.andrew/S2/main.cpp
@@ -1,14 +1,58 @@
 #include <iostream>
 #include <iomanip>
+#include <fstream>
 #include "countFunctions.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-  std::string str;
-  std::getline(std::cin, str);
-  asafov::queue_t queue;
-  asafov::str_to_queue(queue, str);
-  asafov::into_polish(queue);
-  std::cout << std::fixed << std::setprecision(0) << asafov::count(queue) << '\n';
-  return 0;
+  if (argc == 1)
+  {
+    std::string str;
+    std::getline(std::cin, str);
+    asafov::queue_t queue;
+    asafov::str_to_queue(queue, str);
+    asafov::into_polish(queue);
+    std::cout << std::fixed << std::setprecision(0) << asafov::count(queue) << '\n';
+    return 0;
+  }
+  else if (argc == 2)
+  {
+    std::ifstream fin(argv[1]);
+    std::stack<double> result;
+    do
+    {
+      std::string str;
+      std::getline(std::cin, str);
+      asafov::queue_t queue;
+      asafov::str_to_queue(queue, str);
+      asafov::into_polish(queue);
+      result.push(asafov::count(queue));
+    } while (!fin.eof());
+    fin.close();
+    if (result.empty())
+    {
+      std::cout << '\n';
+    }
+    else if (result.size() > 1)
+    {
+      std::cout << result.top() << '\n';
+    }
+    else
+    {
+      std::cout << result.top();
+      result.pop();
+      while (!result.empty())
+      {
+        std::cout << ' ' << result.top();
+        result.pop();
+      }
+      std::cout << '\n';
+    }
+    return 0;
+  }
+  else
+  {
+    std::cerr << "invalid arguments\n";
+    return 1;
+  }
 }
